3e1a4410e6e1439dbf6d565bf41d0dfe17bc9680
pierrecochet <pierre.cochet@outlook.com>
2019-05-15 08:26:33 +0200

    oui1

---
+++ test.py
@@ -0,0 +1,94 @@
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+import tweepy
+import logging
+logging.basicConfig()
+from datetime import datetime, timedelta
+
+# Variables that contains the user credentials to access Twitter API
+
+"""
+ACCESS_TOKEN = '6895181-SqAfibTTTPj426Njs5DoZPw4WOtkDeghHF9cHu8IM'
+ACCESS_SECRET = 'TDSNRDwsmHVxVISOpowpADB6f3SnvAu6eOcLg7PGlwDjn'
+CONSUMER_KEY = 'qFBm7PP8yvBO87EuvGCmQ'
+CONSUMER_SECRET = 'UrVAZEPoFtcFNh20ilEae18yjQ2MdnH44Dz2wtpsKk'
+
+"""
+ACCESS_TOKEN = '1092775756990742528-f3jdO4dHk6mz74xelnaIR5DanAWPm6'
+ACCESS_SECRET = 'ajiXNmSln042ivtOOTh9GYkh0vcJNZwiQAmZMuf6sRCtB'
+CONSUMER_KEY = 'nazYTA9BgmjpZSB54whfr4gkF'
+CONSUMER_SECRET = 'zOm049TtpKJ4zc36gqD3XV8xl4SYSvQJCz1AygEbDK0BVt5v37'
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+# Create the api to connect to twitter with your creadentials
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+#---------------------------------------------------------------------------------------------------------------------
+# wait_on_rate_limit= True;  will make the api to automatically wait for rate limits to replenish
+# wait_on_rate_limit_notify= Ture;  will make the api  to print a notification when Tweepyis waiting for rate limits to replenish
+#---------------------------------------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# This is the equivalent of /timeline/home on the Web.
+#---------------------------------------------------------------------------------------------------------------------
+
+
+def getFollowers(idAccount):
+    list=[]
+    for status in tweepy.Cursor(api.followers_ids, id = idAccount).items(5):
+        list.append(status)
+    return list
+
+
+ids=getFollowers("_agricool")
+
+"""
+listTest=[]
+for i in range (10):
+    listTest.append(ids[i])
+
+print(listTest)
+"""
+
+def setDateT(dateTweet):
+    monthsDic  = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
+    dateToString = dateTweet[8:10]+"/"+monthsDic[dateTweet[4:7]]+"/"+dateTweet[-2:]
+    d = datetime.strptime(dateToString,"%d/%m/%y")
+    h = dateTweet[dateTweet.find(':')-2:dateTweet.find(':')]
+    m = dateTweet[dateTweet.find(':')+1:dateTweet.find(':')+3]
+    d = d.replace(hour=int(h),minute=int(m))
+    d = d + timedelta(hours=1)
+    return d
+
+print(ids)
+
+def getTweet(ids):
+    for id in ids:
+        for status in tweepy.Cursor(api.user_timeline, screen_name=api.get_user(id).screen_name).items(3):
+            print("screen name : "+api.get_user(id).screen_name);
+            tweet = status._json
+            print(setDateT(tweet['created_at']))  # when the tweet posted
+            print(tweet['text'])# content of the tweet
+            print('-----------------------------------------------------------------------------------------------\n')
+
+
+print(getTweet(ids))
+
+
+
+#def getTweetDate(tweet)
+
+#def getTweetType(tweet)
+
+
+
+
+
+
+
